71e612df385485a313670c593fb61babdec54ee5
masonedmison <mason.edmison@gmail.com>
2022-04-06 20:53:21 -0500

    compound product types

---
+++ modules/order-taking/src/main/scala/widgetsinc/ordertaking/common/compound.scala
@@ -0,0 +1,21 @@
+package widgetsinc.ordertaking.common
+package compound
+
+import simple.*
+
+import eu.timepit.refined.types.string.NonEmptyFiniteString
+
+private type String50 = NonEmptyFiniteString[50]
+
+case class PersonalName(firstName: String50, lastName: String50)
+
+case class CustomerInfo(name: PersonalName, email: EmailAddress)
+
+case class Address(
+  line1: String50,
+  line2: Option[String50],
+  line3: Option[String50],
+  line4: Option[String50],
+  city: String50,
+  zipCode: ZipCode
+)
